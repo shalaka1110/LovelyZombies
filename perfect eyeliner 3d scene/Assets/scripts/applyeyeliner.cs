15dba36eeb1a8390cd99bdb57818b1b592c423be
shalaka1110 <40331393+shalaka1110@users.noreply.github.com>
2018-06-17 18:46:40 +0530

    a

--- perfect eyeliner 3d scene/Assets/scripts/applyeyeliner.cs
+++ perfect eyeliner 3d scene/Assets/scripts/applyeyeliner.cs
@@ -28,12 +28,26 @@ public class applyeyeliner : MonoBehaviour
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 15f));
             line.SetPosition(vertexCount, mousePos);
             vertexCount++;
+            BoxCollider2D collider = gameObject.AddComponent<BoxCollider2D>();
+            collider.transform.position = line.transform.position;
+            collider.size = new Vector3(0.1f, 0.1f);
+
         }
         if (Input.GetMouseButtonUp(0))
         {
             mouseDown = false;
             vertexCount = 0;
             line.positionCount = 0;
+            DestroyCollider();
         }
+    }
+    private void DestroyCollider()
+    {
+        BoxCollider2D[] box = GameObject.FindObjectsOfType<BoxCollider2D>();
+        foreach(BoxCollider2D _box in box)
+        {
+            Destroy(_box);
+        }
+
     }
 }
